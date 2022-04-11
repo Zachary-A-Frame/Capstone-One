a606df04da941eeb9aa5c1162e0f88add355c160
Zach <Zacaframe@gmail.com>
2022-04-11 09:33:00 -0400

    Refactored game.py

--- game/game.py
+++ game/game.py
@@ -1,5 +1,5 @@
 from django.db import IntegrityError
-from flask import Blueprint, render_template, g, session, flash, redirect
+from flask import Blueprint, render_template, g, session, flash, redirect, url_for
 from forms import GiveGuessForm
 from models import db, connect_db, User, Movie
 # CSV Reading
@@ -22,61 +22,53 @@ def add_user_to_g():
 
     if CURR_USER_KEY in session:
         g.user = User.query.get(session[CURR_USER_KEY])
-
+        print("Global user")
+        print(g.user)
+        print(g.user.score)
     else:
         g.user = None
 
 # Game main route, this is where players will start. Randomly selects an imdb_id and presents the plot of a film. Plot is uninteractable (Players cannot highlight the text in order to paste it into another window to find the film / answer)
-
-
 @game_bp.route('/', methods=["GET", "POST"])
 def game_home():
-    form = GiveGuessForm()
+    # Make sure user is signed in or signed up.
+    if not g.user:
+        flash("Try signing up or logging in!", "error")
+        return redirect("/auth/signup")
     user = g.user
+    form = GiveGuessForm()
 
-    # Gather data
-    # df = pd.read_csv('csv/movies.csv')
-    # imdb_df = df.imdb
-    # random_imdb_id = imdb_df[random.randint(0, 1795)]
-    # response = requests.get(
-    #     f"http://omdbapi.com/?apikey={APIKEY}&i={random_imdb_id}"
-    # )
-    # data = response.json()
-
-    # Define Variables
-    # plot = data["Plot"]
-    # title = data["Title"]
-    # poster = data["Poster"]
-    # year = data["Year"]
-    # Parse our data range
-    # for source in data['Ratings']:
-    #     if source['Source'] == 'Rotten Tomatoes':
-    #         actual_score = int(source['Value'][:-1])
-    #         actual_score_low = actual_score - 10
-    #         actual_score_high = actual_score + 10
-    # On form submission, get user input and calculate whether user was within +/- 10 of the actual score
+    # Due to API Call restrictions, we have ~600 films.
+    random_num = random.randint(0, 635)
+    movie = Movie.query.get(random_num)
+    # On submit we want to check to see if the player won. There are three cases, for simplicity.
+    # 1. Perfect guess, +25 points
+    # 2. Close, within 10 points, +10 points.
+    # 3. Complete miss. No points awarded.
     if form.validate_on_submit():
         user_input = int(form.guess.data)
-        # score_range = range(actual_score_low, actual_score_high)
+        if user_input == movie.actual_score:
+            user.score = user.score + 25
+            db.session.commit()
+            flash("WOW! You were spot on! 25 Points!", "success")
+            return redirect(f'/movie/{random_num}')
+        elif (user_input >= (movie.actual_score - 10)) and (user_input <= (movie.actual_score + 10)):
+            user.score = user.score + 10
+            db.session.commit()
+            flash("Close enough! 10 Points to Griffindor!", "success")
+            return redirect(f'/movie/{random_num}')
+        else:
+            flash("No such luck, try again!", "error")
+            return redirect(f'/movie/{random_num}')
 
-        # if user_input in score_range:
-        #     user.score = user.score + 10
-        #     db.session.commit()
-        #     return redirect("/movie/{data.imdb}")
-            # return render_template("answer.html", title=title, plot=plot, year=year, poster=poster, actual_score=actual_score)
-
-    return render_template('game.html', plot=plot, form=form, user=user, title=title, actual_score=actual_score, actual_score_low=actual_score_low)
+    return render_template('game.html', form=form, user=user, movie=movie)
 
 
 @game_bp.route('/movie/<int:movie_id>', methods=["GET", "POST"])
 def game_answer_page(movie_id):
     """Show Movie Page"""
+    user = g.user
     movie = Movie.query.get_or_404(movie_id)
-    response = requests.get(
-        f"http://omdbapi.com/?apikey={APIKEY}&i={movie.imdb}"
-    )
-    data = response.json()
-    plot = data["Plot"]
-    poster = data["Poster"]
-
-    return render_template('movie.html', movie=movie, plot=plot, poster=poster)
+    print("User score")
+    print(user.score)
+    return render_template('movie.html', movie=movie, user=user)
