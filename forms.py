50da3a8225cda732c9c1043898dfb13bc301133c
Zach <Zacaframe@gmail.com>
2022-04-24 21:21:23 -0400

    Added value restrictions between 0-100 on guesses.

--- forms.py
+++ forms.py
@@ -1,10 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, IntegerField, HiddenField
 from wtforms.validators import DataRequired, Email, Length
+from wtforms.widgets.html5 import NumberInput
+
 
 class GiveGuessForm(FlaskForm):
     """Form for users to submit guess"""
-    guess = StringField('Guess!', validators=[
+
+    guess = StringField('Guess!', widget=NumberInput(), render_kw={'max': "100", 'min': '0'}, validators=[
                         DataRequired(), Length(min=0, max=3)])
     movie_id = HiddenField("Movie Id")
 
